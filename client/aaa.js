0cc6ac401551d8e905bd51fbe1cb58d0426af4a8
limabeans <toomanytypos@gmail.com>
2015-08-16 00:56:22 -0700

    users maybe

--- client/aaa.js
+++ client/aaa.js
@@ -1,2 +1,3 @@
 Meteor.subscribe("rooms");
 Meteor.subscribe("messages");
+
--- client/home.js
+++ client/home.js
@@ -1,5 +1,10 @@
 Template.home.helpers({
   some_list: [
     {type: "cat"}, {type:"dog"}, {type:"carrot"}
-  ]
+  ],
+  list_users: function() {
+    console.log(Meteor.users.find());
+    console.log(Meteor.users.find().fetch());
+    return Meteor.users.find();
+  }
 });
