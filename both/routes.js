0947255dc5f96fea30d24a72d041b2188e0d1c21
limabeans <toomanytypos@gmail.com>
2015-08-18 22:14:14 -0700

    at finally working, but probs need to clean up now

--- both/routes.js
+++ both/routes.js
@@ -19,3 +19,12 @@ Router.route('/rooms', function() {
 Router.route('/main_room', function() {
   this.render('main_room');
 });
+
+
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('enrollAccount');
+AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('verifyEmail');
--- client/home.js
+++ client/home.js
@@ -3,17 +3,10 @@ Template.home.helpers({
     {type: "cat"}, {type:"dog"}, {type:"carrot"}
   ],
   list_users: function() {
-    var users = Meteor.users.find().fetch();
-    // extract out the emails
-    users = users.map(function(user) {return user.emails[0].address;});
-    return users;
+    return Meteor.users.find();
   },
   list_online_users: function() {
-    var users = Meteor.users.find({'status.online': true});
-    // extract out the emails
-    users = users.map(function(user) {return user.emails[0].address;});
-    return users;
-
+    return Meteor.users.find({'status.online': true});
   }
 });
 
